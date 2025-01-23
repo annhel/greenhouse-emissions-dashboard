b5e56086f80943f38c854978f572062cf2745d73
Angel Martinez <angel.martinez@nodeitx.com>
2025-01-23 13:08:51 -0500

    Changes to HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,17 @@ import React, { useState } from "react";
 import OverviewPage from "./pages/OverviewPage";
 import NavPane from "./components/NavPane/NavPane";
 import ComparePage from "./pages/ComparePage";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  const [currentPage, setCurrentPage] = useState<string>("Overview");
-
   return (
     <span style={{ display: "flex" }}>
-      <BrowserRouter basename="/greenhouse-emissions-dashboard">
+      <HashRouter basename="/greenhouse-emissions-dashboard">
         <Routes>
           <Route path="/" element={<OverviewPage />} />
           <Route path="/compare" element={<ComparePage />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </span>
   );
 }
