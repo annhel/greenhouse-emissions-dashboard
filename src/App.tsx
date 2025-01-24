d5d3551236ef7fd60e1945013abc1525addb0905
Angel Martinez <angel.martinez@nodeitx.com>
2025-01-23 19:18:23 -0500

    Switches to BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ComparePage from "./pages/ComparePage";
 import OverviewPage from "./pages/OverviewPage";
 
 function App() {
   return (
     <span style={{ display: "flex" }}>
-      <HashRouter basename="/greenhouse-emissions-dashboard">
+      <BrowserRouter basename="/greenhouse-emissions-dashboard">
         <Routes>
           <Route path="/" element={<OverviewPage />} />
           <Route path="/compare" element={<ComparePage />} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </span>
   );
 }
